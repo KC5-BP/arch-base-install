45c45bab172c22e91bce07bb789431911cdcd777
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2023-05-28 14:47:56 +0200

    Add alsa-oss pkg for dmix application

--- scripts/base-whilechroot.sh
+++ scripts/base-whilechroot.sh
@@ -3,21 +3,21 @@
 # VARIABLES
 LANGUAGE=en_US.UTF-8
 KEYMAP_LAYOUT=fr_CH-latin1
-HOSTNAME=mmmmmh
-BOOTLOADER_DIR_NAME=ArchSys
-ENCRYPTED=true
+HOSTNAME=theShipwreck
+BOOTLOADER_DIR_NAME=shipwreckGrub
+ENCRYPTED=false
 
 echo "Setting up TIME ZONE"
 ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 
 #echo "Setting up DATE & CLOCK to hardware"
-#hwclk --systohc
+hwclock --systohc
 
 echo "Setting up LOCALIZATION"
 echo "For $LANGUAGE"
-sed -i '177s/.//' /etc/locale.gen 
+sed -i '171s/.//' /etc/locale.gen 
 echo "For fr_CH.UTF-8"
-sed -i '246s/.//' /etc/locale.gen
+sed -i '240s/.//' /etc/locale.gen
 locale-gen
 echo "Putting language in locale.conf"
 echo "LANG=$LANGUAGE" >> /etc/locale.conf
@@ -34,48 +34,48 @@ echo "Setting up ROOT PASSWORD"
 echo root:password | chpasswd
 
 echo "Installing base pkgs"
-pacman -S grub grub-btrfs efibootmgr os-prober \
+pacman -S grub efibootmgr os-prober \
 	mtools dosfstools ntfs-3g nfs-utils \
 	iwd networkmanager network-manager-applet openssh wireless_tools wpa_supplicant \
 	gdisk parted tlp \
 	cups dialog xdg-user-dirs xdg-utils \
-	alsa-utils bluez bluez-utils pulseaudio-bluetooth pavucontrol \
+	alsa-utils alsa-oss bluez bluez-utils pulseaudio-bluetooth pavucontrol \
 	bash-completion neofetch \
-	cmake gdb nodejs
-echo "Workaround if pacman failed because of marginal trust"
-rm -rf /etc/pacman.d/gnupg
-pacman-key --init && pacman-key --populate && \
-	pacman-key -r 139B09DA5BF0D338 && pacman-key --lsign-key 139B09DA5BF0D338 
-echo "2nd try on installing  base pkgs"
-pacman -S grub grub-btrfs efibootmgr os-prober \
-	mtools dosfstools ntfs-3g nfs-utils \
-	iwd networkmanager network-manager-applet openssh wireless_tools wpa_supplicant \
-	gdisk parted tlp \
-	cups dialog xdg-user-dirs xdg-utils \
-	alsa-utils bluez bluez-utils pulseaudio-bluetooth pavucontrol \
-	bash-completion neofetch \
-	cmake gdb nodejs
+	cmake gdb ninja
+#echo "Workaround if pacman failed because of marginal trust"
+#rm -rf /etc/pacman.d/gnupg
+#pacman-key --init && pacman-key --populate && \
+#	pacman-key -r 139B09DA5BF0D338 && pacman-key --lsign-key 139B09DA5BF0D338 
+#echo "2nd try on installing  base pkgs"
+#pacman -S grub grub-btrfs efibootmgr os-prober \
+#	mtools dosfstools ntfs-3g nfs-utils \
+#	iwd networkmanager network-manager-applet openssh wireless_tools wpa_supplicant \
+#	gdisk parted tlp \
+#	cups dialog xdg-user-dirs xdg-utils \
+#	alsa-utils bluez bluez-utils pulseaudio-bluetooth pavucontrol \
+#	bash-completion neofetch \
+#	cmake gdb nodejs
 
 echo "Installing GPU drivers"
 #pacman -S xf86-video-qxl # Virtual Machine
-#pacman -S xf86-video-intel mesa
+pacman -S xf86-video-intel mesa
 #pacman -S xf86-video-amdgpu
-#pacman -S nvidia nvidia-utils nvidia-settings 
+pacman -S nvidia nvidia-utils nvidia-settings 
 
 echo "Enabling services"
 systemctl enable NetworkManager
-systemctl enable bluetooth 
+systemctl enable bluetooth
 systemctl enable cups
-systemctl enable sshd 
-systemctl enable iwd 
+systemctl enable sshd
+systemctl enable iwd
 systemctl enable tlp
 systemctl enable reflector.timer
 
 echo "Creating a basic user"
-useradd -m user
-echo user:password | chpasswd
-#usermod -aG <GROUPS> user # If needed to add user to a specific group
-echo "user ALL=(ALL) ALL" >> /etc/sudoers.d/user
+useradd -m deep
+echo deep:password | chpasswd
+usermod -aG wheel deep # If needed to add user to a specific group
+echo "deep ALL=(ALL) ALL" >> /etc/sudoers.d/deep
 
 echo "Creating bootloader"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=$BOOTLOADER_DIR_NAME
